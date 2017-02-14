d23a9004f069b543b6339732f53ab71a980990b2
1ahmadzoh <1ahmadzoh@GWS03915.ihdsb.CA>
2017-02-14 14:45:44 -0500

    February - 14 - 2017 ------------------------------------------------ Worked on RottenTomatoes part one , finished most of it.

--- src/edu/hdsb/gwss/zohaib/ics4u/u1/RottenTomatoesP1.java
+++ src/edu/hdsb/gwss/zohaib/ics4u/u1/RottenTomatoesP1.java
@@ -12,6 +12,7 @@ package edu.hdsb.gwss.zohaib.ics4u.u1;
 
 import java.util.Scanner;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 
 /**
@@ -23,20 +24,47 @@ public class RottenTomatoesP1 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         //Objects
-        
+
         Scanner input = new Scanner(System.in);
         
-       // Scanner fileReader = new Scanner ( file ());
-        
-       //Intro
-       System.out.println("Movie Reviews \n" 
-              + "--------------------------------------------------------"  );
-       
-       System.out.println("Enter a word : ");
-       
-       
-    }
+        Scanner fileReader = new Scanner(new FileReader("MovieReviews.txt"));
 
+        //Variables
+        String word;
+        
+        int repeat;
+        
+        double moviereview;
+        
+        String line = fileReader.nextLine();
+
+        //Intro
+        System.out.println("Movie Reviews \n"
+                + "--------------------------------------------------------");
+        
+        System.out.print("Enter a word : ");
+        
+        word = input.nextLine();
+        
+        repeat = 0;
+
+        // Loop
+        while (fileReader.hasNext()) {
+            
+            if (fileReader.next().toUpperCase().equals(word.toUpperCase())) {
+                repeat++;
+               // fileReader.index(0);
+            }
+            
+        }
+        
+        //Calculation for average 
+        
+        System.out.println("The word " + " ' " + word + " ' " + " repeats " + repeat
+                + " times");
+        
+    }
+    
 }
