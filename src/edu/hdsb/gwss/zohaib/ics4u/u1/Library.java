f59a4618be04521932a5774491db39e8fa3622db
1ahmadzoh <1ahmadzoh@hdsb.ca>
2017-02-26 15:34:53 -0500

    Sunday,February, 26,2017
    --------------------------------------------------------
    Rotten Tomatoes is ready to be marked.

--- src/edu/hdsb/gwss/zohaib/ics4u/u1/Library.java
+++ src/edu/hdsb/gwss/zohaib/ics4u/u1/Library.java
@@ -11,10 +11,12 @@
 package edu.hdsb.gwss.zohaib.ics4u.u1;
 
 import java.io.FileNotFoundException;
+
 import java.io.FileReader;
+
 import java.util.Scanner;
-import java.util.StringTokenizer;
-import java.math.BigDecimal;
+
+import java.util.Arrays;
 
 /**
  *
@@ -65,28 +67,39 @@ public class Library {
         Scanner fileReader = new Scanner(new FileReader("MovieReviews.txt"));
 
         //Variables
+        //Variable that holds how many times the while loop will repeat
         double repeat = 0;
 
+        //Variable that will be used to calculate moviereviewaverage
         double moviereview = 0;
 
+        //Stores value of moviereview average that will be used to print out value for user
         double moviereviewaverage = 0;
 
+        //Hold value of the file of which will specify which file to calculate moviereview average
         String wordfile;
 
         // Loop
         while (fileReader.hasNext()) {
 
+            //While filereader the file has a token in its input wordfile will equal the files next line
             wordfile = fileReader.nextLine();
 
+            //If wordfile the files next line to upper case contains the word the user specified to upper case do this statement
             if (wordfile.toUpperCase().contains(word.toUpperCase())) {
 
+                //If file reader has a double that do these calculations
                 if (fileReader.hasNextDouble()) {
 
+                    //moviereview is equal to the files next double the number at the front of each file
                     moviereview = fileReader.nextDouble();
 
+                    //Each time the if statement is true increase repeat by one
                     repeat++;
 
                 }
+
+                //Accumulate moviereview and add it to the value of moviereviewaverage
                 moviereviewaverage += moviereview;
             }
 
@@ -95,38 +108,174 @@ public class Library {
         //Calculation for average
         moviereviewaverage = (moviereviewaverage / repeat);
 
+        //Return the value of moviereviewaverage
         return moviereviewaverage;
 
     }
 
     //Review method (part two)
     public static double sentencereview(String file) throws FileNotFoundException {
-        //Object
+        //Objects
 
         Scanner negReader = new Scanner(new FileReader("NegativeReview.txt"));
 
+        Scanner wordReader = new Scanner(new FileReader("WordList.txt"));
+
         //Variables
+        //Holds the value of moviereviewaverage and repeat as 0
         double moviereviewaverage = 0, repeat = 0;
 
+        //Holds variables words of which will be each line of the file under string
         String words;
 
-        if (file.equals("negTest.txt")) {
+        //If the file is equal to this string then do this
+        if (file.equals("negtest.txt")) {
 
+            //While the file has a token do this
             while (negReader.hasNext()) {
 
+                //words is equal to the files next line
                 words = negReader.nextLine();
 
+                //put the word through the method .wordreview and add the value returned each time to moviereviewaverage
                 moviereviewaverage += Library.wordreview(words);
 
+                //every time there is a token add one to repeat
                 repeat++;
 
             }
 
-            moviereviewaverage = (moviereviewaverage) / (repeat);
+            //if the file is not equal to 'negtest.txt' see if the file is equal to 'words.txt' and if it is do this
+        } else if (file.equals("words.txt")) {
 
-         
+            //While the file has a token
+            while (wordReader.hasNext()) {
+
+                //words is equal to the files next line
+                words = wordReader.nextLine();
+
+                //put the word through the method .wordreview and add the value returned each time to moviereviewaverage
+                moviereviewaverage += Library.wordreview(words);
+
+                //every time there is a token add one to repeat
+                repeat++;
+
+            }
+        }
+
+        //Calculations for moviereviewaverage : moviereviewaverage is equal to moviereviewaverage value divided by repeat value
+        moviereviewaverage = (moviereviewaverage) / (repeat);
+
+        //Return moviereviewaverage everytime this method is used with specified perameter
+        return moviereviewaverage;
+    }
+
+    //Review Method Multiple Choice(Part 3)
+    public static void multiplechoice(String file) throws FileNotFoundException {
+        //Object
+
+        Scanner negReader = new Scanner(new FileReader("NegativeReview.txt"));
+
+        Scanner wordReader = new Scanner(new FileReader("WordList.txt"));
+
+        //Variables
+        //Declares a valuable int 'repeat' as 0 for later calculations
+        int repeat = 0;
+
+        //Stores word and original word into string inorder to retain value of word once it has changed later
+        String word, originalword;
+
+        //Declares two double arrays and two string arrays
+        double[] negArray;
+        double[] wordArray;
+
+        String[] negArrays;
+        String[] wordArrays;
+
+        //If file inputed by user is equal to this then do this
+        if (file.equals("negtest.txt")) {
+
+            //Declares negarray as a double with size of 8 and negarrays as a string with size of 8
+            negArray = new double[8];
+
+            negArrays = new String[8];
+
+            //While file has next do this
+            while (negReader.hasNext()) {
+
+                //word is equal to file next line
+                word = negReader.nextLine();
+
+                //Incase user inputs a capital put word into lowercase
+                word = word.toLowerCase();
+
+                //original word is equal to word before word is changed
+                originalword = word;
+
+                //Stores original word into string array
+                negArrays[repeat] = originalword;
+
+                //Stores values of word scores that have been calculated using method into double array
+                negArray[repeat] = Library.wordreview(word);
+
+                //Add one to repeat everytime file has a token
+                repeat++;
+
+            }
+
+            //Sorts values from least to greatest
+            Arrays.sort(negArrays);
+
+            //Sorts values from least to greatest
+            Arrays.sort(negArray);
+
+            //Gives information of what user asked for
+            System.out.println(" The most positive word , with a score of  " + negArray[(negArray.length) - 1] + " is "
+                    + negArrays[(negArray.length) - 1] + " \n The most negative word , with a score of  " + negArray[0] + " is "
+                    + negArrays[0]);
+
+            //If file is not equal to "negtest.txt" see if they inputted "words.txt"
+        } else if (file.equals("words.txt")) {
+
+            //Declares wordarray as a double with size of 8 and wordarrays as a string with size of 8
+            wordArray = new double[44];
+
+            wordArrays = new String[44];
+            
+            //While file has a next line do this
+            while (wordReader.hasNext()) {
+                
+                //word is equal to file next line
+                word = wordReader.nextLine();
+                
+                //word is equal to word but lower cased
+                word = word.toLowerCase();
+                
+                //retain original value of word before its changed
+                originalword = word;
+                
+                //Creates array to store values of orginal word and word
+                wordArrays[repeat] = originalword;
+
+                wordArray[repeat] = Library.wordreview(word);
+                
+                //every time the file has a token add one to repeat
+                repeat++;
+
+            }
+            
+            //Sorts the values gained from while loop from least to greatest
+            Arrays.sort(wordArray);
+
+            Arrays.sort(wordArrays);
+            
+            //Prints out the information user wants
+            System.out.println(" The most positive word , with a score of  " + wordArray[(wordArray.length) - 1] + " is "
+                    + wordArrays[(wordArray.length) - 1] + " \n The most negative word , with a score of  " + wordArray[0] + " is "
+                    + wordArrays[0]);
+
+        }
 
     }
-        return moviereviewaverage;
-}
+
 }
--- src/edu/hdsb/gwss/zohaib/ics4u/u1/RottenTomatoesP1.java
+++ src/edu/hdsb/gwss/zohaib/ics4u/u1/RottenTomatoesP1.java
@@ -3,17 +3,18 @@
  
  + * Date : Monday , February 13 , 2017
  
- + * Version : v0.01
+ + * Version : v0.04
 
- + * Description : Rotten Tomatoes Part One
+ + * Description : Rotten Tomatoes Except Part 4 
 
  */
 package edu.hdsb.gwss.zohaib.ics4u.u1;
 
+//Imports scanner of which is used in program to get input values from user
 import java.util.Scanner;
-import java.io.File;
+
+//File not found : tells java to ignore if file is not found
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 
 public class RottenTomatoesP1 {
 
@@ -26,79 +27,117 @@ public class RottenTomatoesP1 {
 
         Scanner input = new Scanner(System.in);
 
-        Scanner wordReader = new Scanner(new FileReader("WordList.txt"));
-
         //Variables
+        //String Variable that holds file name and word , file name and word that is specified by user
         String word, file;
 
-        double moviereviewaverage = 0, repeat = 0;
+        //A double that keeps value of moviereviewaverage
+        double moviereviewaverage = 0;
+
+        //Variable for choice (initialize
+        int choice = 0;
 
         //Intro
+        //Title
         System.out.println("Movie Reviews \n"
                 + "--------------------------------------------------------");
 
-        System.out.print("Enter a word : ");
+        //Part(5) Menu : Creates options for user to pick what they want to do
+        //While loop to reopen menu till user presses 5
+        while (choice != 5) {
+            System.out.println("\n" + "What would you Like to do?");
 
-        word = input.nextLine();
+            System.out.println("1:Get The Score of a word");
 
-        System.out.println(word + " appears " + repeat + " times" + " \n"
-                + "The average score for reviews containing the word " + word + " is " + Library.wordreview(word));
+            System.out.println("2:Get the average score of words in a file(one word per line)");
 
-        //Part Two
-        //Intro
-        System.out.print("Enter the name of the file with words you want to find the average score for : ");
+            System.out.println("3:Find the highest/lowest scoring words in a file");
 
-        //Retains value of name of file from user
-        file = input.nextLine();
+            System.out.println("4: Sort words from a file into positive.txt and negative.txt");
 
-        //Calls upon a method to determine if overall negative review average or overall positive
-        moviereviewaverage = Library.sentencereview(file);
+            System.out.println("5:Exit the program");
 
-        if (moviereviewaverage >= 2.01) {
-            System.out.println("The overall score of words in " + file + " is " + moviereviewaverage
-                    + "\nThe overall sentiment of " + file + " is positive ");
+            System.out.print(" Enter a number 1 - 5: ");
 
-        } else if (moviereviewaverage <= 1.99) {
-            System.out.println("The overall score of words in " + file + " is " + moviereviewaverage
-                    + " \n The overall sentiment of " + file + " is negative");
-        } else {
-            System.out.println("file does not exist");
-        }
+            choice = input.nextInt();
 
-        //Part Three
-        //Intro
-        System.out.print("Enter the name of the file with words you want to score: ");
+            //Part One : Choice One
+            switch (choice) {
+                case 1:
+                    //Prompts user to enter word
+                    System.out.print("Enter a word : ");
 
-        file = input.nextLine();
+                    //Takes the value of what user entered and stores it into word
+                    word = input.next();
 
-        //Variables
-        int positive = 0, negative = 0;
+                    //Calculates average scoring through method from library and tells user the given information
+                    System.out.println("The average score for reviews containing the word " + word + " is " + Library.wordreview(word));
 
-        int i = 0, a = 0;
+                    break;
 
-        double[] filesize;
+                //Part Two : Choice Two
+                case 2:
 
-        while (wordReader.hasNext()) {
-            wordReader.next();
-            i++;
+                    //Intro
+                    //Prompts user to enter a name of file they would like to find average score of 
+                    System.out.print("Enter the name of the file with words you want to find the average score for : ");
 
-        }
+                    //Retains value of name of file from user
+                    file = input.next();
 
-        //Array
-        filesize = new double[i];
+                    //In case user puts in any capitals for file name it converts it back into lower case
+                    file = file.toLowerCase();
 
-        if (file.equals("words.txt")) {
-            a++;
-            while (wordReader.hasNext()) {
+                    //Calls upon a method to determine if overall negative review average or overall positive
+                    moviereviewaverage = Library.sentencereview(file);
 
-                word = wordReader.nextLine();
+                    //If movie review average is greater than or equal to 2.01 then the overall sentiment is positive in the file specified
+                    if (moviereviewaverage >= 2.01) {
+                        System.out.println("The overall score of words in " + file + " is " + moviereviewaverage
+                                + "\nThe overall sentiment of " + file + " is positive ");
 
-                filesize[a] = Library.sentencereview(word);
+                        //If movie review average is less than or equal to 1.99 than overall sentiment is negative in the file specified
+                    } else if (moviereviewaverage <= 1.99) {
+                        System.out.println("The overall score of words in " + file + " is " + moviereviewaverage
+                                + " \n The overall sentiment of " + file + " is negative");
 
-                System.out.print(filesize[a]);
+                        //If user inputs a file that does not exist
+                    } else {
+                        System.out.println("File does not exist");
+                    }
+
+                    break;
+
+                //Part Three : Choice Three
+                case 3:
+
+                    //Intro
+                    //Prompts user to enter name of file they would like to score
+                    System.out.print("Enter the name of the file with words you want to score: ");
+
+                    //takes the input of the user and stores it into the variable "file"
+                    file = input.next();
+
+                    //Calls the method from Library to calculate for score and of which lists the score user wants
+                    Library.multiplechoice(file);
+
+                    break;
+
+                //Part Four : Choice Four
+                case 4:
+
+                    System.out.println("Not available");
+
+                    break;
+
+                //Part Five: Choice Five
+                default:
+
+                    System.out.println("");
 
             }
 
         }
+
     }
 }
