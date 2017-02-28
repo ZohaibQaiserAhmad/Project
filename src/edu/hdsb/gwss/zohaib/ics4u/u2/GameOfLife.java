2d07aa795456621c5fb5f7bbddc9954332a556a4
1ahmadzoh <1ahmadzoh@GWS03915.ihdsb.CA>
2017-02-28 14:42:05 -0500

    Tuesday, February 28th
    Created File GameOfLife

---
+++ src/edu/hdsb/gwss/zohaib/ics4u/u2/GameOfLife.java
@@ -0,0 +1,26 @@
+/*
+ +* Name: Zohaib Ahmad
+ 
+ + * Date : Tuesday , 28 Feburary 
+ 
+ + * Version : v0.01
+
+ + * Description : 
+
+ */
+package edu.hdsb.gwss.zohaib.ics4u.u2;
+
+/**
+ *
+ * @author 1ahmadzoh
+ */
+public class GameOfLife {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
