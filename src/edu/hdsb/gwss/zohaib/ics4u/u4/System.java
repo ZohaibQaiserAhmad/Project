3a7d522f4be23f0f0aba1bd6362b92254790674b
1ahmadzoh <1ahmadzoh@GWS03915.ihdsb.CA>
2017-04-20 14:50:25 -0400

    April-20-17 ------------- Worked on object assignment.

---
+++ src/edu/hdsb/gwss/zohaib/ics4u/u4/System.java
@@ -0,0 +1,9 @@
+package edu.hdsb.gwss.zohaib.ics4u.u4;
+
+import java.util.ArrayList;
+
+public class System {
+    
+    private ArrayList<VideoGame> games;
+    
+}
---
+++ src/edu/hdsb/gwss/zohaib/ics4u/u4/VideoGame.java
@@ -0,0 +1,8 @@
+public class VideoGame {
+    
+    private System sytem;
+    
+    
+    
+}
+
